edc6f1c07541cf8ac999fa74d37797a3260785c8
Maria Fernanda Sarrelli <mfsarrelli@gmail.com>
2025-10-06 20:07:54 -0300

    subida de los dos primeros archivos de la pre entrega

---
+++ test/test_saucedemo.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+
+
+@pytest.fixture
+def driver():
+    # configuracion para consultar a selenium web driver
+def test_login():
+    #logueo de usuario 
+    #click boton de login
+    #assert la palabra "inventory" dentro de la URL y el titulo "Swag Labs"
+
+def test_catalogo():
+    #logueo de usuario
+    #click boton de login
+    #assert si existen productos visibles en la pagina (lenght()>0)
+    #verificar elementos importantes de la pagina
+
+def test_carrito():
+    #logueo de usuario
+    #click boton de login
+    #llevarme a la pagina de carrito de compras
+    #inspeccionar elemento carrito, con su class name descender al hijo y buscar el span que tiene el numero de elementos
+    #comprobar que el carrito agrego el producto

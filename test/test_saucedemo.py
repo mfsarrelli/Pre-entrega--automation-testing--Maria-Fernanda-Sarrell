3ae5627309b2f2ba7b78d6abfce9728af2f9e8a8
Maria Fernanda Sarrelli <mfsarrelli@gmail.com>
2025-10-15 19:06:03 -0300

    actualizacion con funciones login, catalogo y carrito

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -1,23 +1,63 @@
 import pytest
+from selenium.webdriver.common.by import By
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+
+from utils.helpers import login_saucedemo, get_driver
 
 
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def driver():
     # configuracion para consultar a selenium web driver
-def test_login():
+    driver = get_driver()
+    yield driver
+    driver.quit()
+
+
+
+def test_login(driver):
+
+    login_saucedemo(driver)
+    assert "/inventory.html" in driver.current_url
+    titulo = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container.title').text
+    assert titulo == 'Products'
+
     #logueo de usuario 
     #click boton de login
     #assert la palabra "inventory" dentro de la URL y el titulo "Swag Labs"
 
-def test_catalogo():
+##def test_catalogo():
     #logueo de usuario
     #click boton de login
     #assert si existen productos visibles en la pagina (lenght()>0)
     #verificar elementos importantes de la pagina
 
+def test_catalogo(driver):
+    login_saucedemo (driver)
+
+    productos = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(productos)>0
+
+
+
 def test_carrito():
+
+    login_saucedemo (driver)
+    productos = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    total_products = len(productos)
+
+    if total_products >= 2:
+        productos[0].find_element(By.TAG_NAME, 'button').click()
+        productos[1].find_element(By.TAG_NAME, 'button').click()
+        
+        badge = driver.find_elements(By.CLASS_NAME, 'shopping_cart_badge').text
+        assert badge == 2
+
+
     #logueo de usuario
     #click boton de login
     #llevarme a la pagina de carrito de compras
---
+++ utils/helpers.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+##from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+
+URL = 'https://www.saucedemo.com'
+USERNAME = 'standard_user'
+PASSWORD =  'secret_sauce'
+
+def get_driver():
+
+    ##options = Options()
+    ##options.add_argument('--start-maximized')
+
+
+    #instalacion del driver
+    service = service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    time.sleep(5)
+    return driver
+
+
+
+def login_saucedemo( driver ):
+    driver.get(URL)
+
+    ##INGRESAR LAS CREDENCIALES
+    driver.find_element(By.NAME, 'user-name').send_keys(USERNAME)
+    driver.find_element(By.NAME, 'password').send_keys(PASSWORD)
+    driver.find_element(By.ID, 'login-button').click()
+
+    time

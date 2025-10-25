9055aa1ab3be0fe23f38c749d7e0fc6d9f545a08
Maria Fernanda Sarrelli <mfsarrelli@gmail.com>
2025-10-25 17:47:36 -0300

    correcciones para que el ultimo assert del carrito == PASSED

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -1,8 +1,10 @@
 import pytest
 
 from selenium.webdriver.common.by import By
-import sys
-import os
+import sys ##sirve para encontrar archivos dentro de mi root
+import os ##transforma rutas relativas cleen absolutas
+
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from utils.helpers import login_saucedemo, get_driver
@@ -24,18 +26,13 @@ def test_login(driver):
 
     login_saucedemo(driver)
     assert "/inventory.html" in driver.current_url
-    titulo = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container.title').text
+    titulo = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
     assert titulo == 'Products'
 
     #logueo de usuario 
     #click boton de login
     #assert la palabra "inventory" dentro de la URL y el titulo "Swag Labs"
 
-##def test_catalogo():
-    #logueo de usuario
-    #click boton de login
-    #assert si existen productos visibles en la pagina (lenght()>0)
-    #verificar elementos importantes de la pagina
 
 def test_catalogo(driver):
     login_saucedemo (driver)
@@ -43,20 +40,24 @@ def test_catalogo(driver):
     productos = driver.find_elements(By.CLASS_NAME, 'inventory_item')
     assert len(productos)>0
 
+##def test_catalogo():
+    #logueo de usuario
+    #click boton de login
+    #assert si existen productos visibles en la pagina (lenght()>0)
+    #verificar elementos importantes de la pagina
 
-
-def test_carrito():
+def test_carrito(driver):
 
     login_saucedemo (driver)
     productos = driver.find_elements(By.CLASS_NAME, 'inventory_item')
-    total_products = len(productos)
+    total_productos = len(productos)
 
-    if total_products >= 2:
+    if total_productos >= 2:
         productos[0].find_element(By.TAG_NAME, 'button').click()
         productos[1].find_element(By.TAG_NAME, 'button').click()
         
-        badge = driver.find_elements(By.CLASS_NAME, 'shopping_cart_badge').text
-        assert badge == 2
+        badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+        assert badge == "2"
 
 
     #logueo de usuario
